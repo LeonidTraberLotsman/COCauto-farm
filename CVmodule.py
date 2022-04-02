1227cb79ddf29b5dd0905a81020d2cde65d657c4
lotsman-1 <leonidt@lotsman.org>
2022-04-02 19:14:49 +0500

    Recognise

--- CVmodule.py
+++ CVmodule.py
@@ -1,26 +1,68 @@
+from tkinter import E
 import cv2 as cv2
 import numpy as np
 import time
+import pytesseract
+
+pytesseract.pytesseract.tesseract_cmd = 'D:\\Unity\\Repos\\COCauto-farm\\goblinsscrean\\tesseract.exe'
+config = r'--oem 3 --psm 6'
 
 file_path='D:\\Unity\\Repos\\COCauto-farm\\'
+
+def Recognise(img):
+    data = pytesseract.image_to_data(img, config=config)
+    finaltext=""
+    for i, el in enumerate(data.splitlines()):
+        if i==0:
+            continue
+
+        e=el.split()
+        
+        
+    
+        if len(e)>11:
+            finaltext+=e[11]
+
+    #------------------
+    digital_str=''
+    for symbol in finaltext:
+        snum=ord(symbol)
+        if snum>47 and snum<58:
+            digital_str+=symbol;
+    return int(digital_str)
 
 def CheckResources():
     time.sleep(2)
     #screen=pg.screenshot()
     screen=cv2.imread(file_path+'example.png')
+    
     y=150
     h=200
     x=100
     w=314
     img = screen[y:h, x:w]
+    
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    
     #---------------
-    hsv_min = np.array((0, 125, 0), np.uint8)
-    hsv_max = np.array((255, 255, 255), np.uint8)
+    hsv_min = np.array((0, 5, 248), np.uint8)
+    hsv_max = np.array((180, 255, 255), np.uint8)
+
+    h=54
+    s=19*2.55
+    v=98*2.55
+    de=200
+    #hsv_min = np.array((h-de,s-de ,v-de), np.uint8)
+    #hsv_max = np.array((h+de,s+de ,v+de), np.uint8)
 
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV )
     thresh = cv2.inRange(hsv, hsv_min, hsv_max)
+    inverted = cv2.bitwise_not(thresh)
+    #inverted=cv2.invert(thresh)
     cv2.imshow("thresh", thresh)
+    cv2.imshow("inverted", inverted)
+    t=Recognise(inverted)
+    print(t)
     #---------------
     #ret, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY)
     img_erode = cv2.erode(thresh, np.ones((3, 3), np.uint8), iterations=1)
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pyautogui as pg
+import random as r
 
 
 import time
@@ -42,6 +43,7 @@ def CanFind(Name):
         x, y = pg.locateCenterOnScreen(Name)  
     except Exception:
             res=False
+    print("can find "+Name+" "+str(res))
     return res
 
 
@@ -60,24 +62,60 @@ def Hire():
         time.sleep(0.1)
     GetToCorner()
     ClickButton(localpath+'g1.png',0.1,0)
-    ClickButton(localpath+'g2.png',0.1,0)
-    while(CanFind(localpath+'g3.png')):
-        ClickButton(localpath+'g3.png',0.1,7)
-    ClickButton(localpath+'g4.png',0.1,0)
+    pg.moveTo(551,90)
+    time.sleep(0.1)
+    pg.click()
+    #ClickButton(localpath+'g2.png',0.1,0)
+    #while(CanFind(localpath+'g3.png')):
+    pg.moveTo(405,825)
+    pg.mouseDown()
+    time.sleep(5)
+    pg.mouseUp()
+    pg.moveTo(1720,90)
+    pg.click()
 
 def ScanEnemies():
+    t=True
     #ClickButton(file_path+"BL.png",2)
     #ClickButton(file_path+"B0.png",2)
-    ClickButton(file_path+"B1.png",1,0)
-    ClickButton(file_path+"B2.png",1,0)
-    time.sleep(2)
-    while(True):
-        ClickButton(file_path+"B3.png",5,0)
-   
+    #Цикл боя
+    while True:
+        ClickButton(file_path+"B1.png",1,0)
+        ClickButton(file_path+"B2.png",1,0)
+        time.sleep(2)
+        while True:
+            if r.randint(0,2)==0:
+                ClickButton(file_path+"B3.png",5,0)
+            else:
+                SpawnGoblins()
+                break
+        Hire()
+        time.sleep(250)
+
+
+
+def SpawnGoblins():
+    GetToCorner()
+    ClickButton(file_path+"attack/A0.png",0.1,0)
+    while CanFind(file_path+"attack/A3.png"):
+        for i in range(5):
+            time.sleep(0.0001)
+            x=r.randint(288,1655)
+            y=r.randint(40,742)
+            pg.moveTo(x,y)
+            pg.click()
+    ClickButton(file_path+"attack/A1.png",1,0)
+    #1010,760
+    #1274,244
+    
+
 
 
     #time.sleep(5)
     #x, y = pg.locateCenterOnScreen(file_path+'Button3.png')
     #pg.moveTo(x,y)
     #pg.click()
-    #time.sleep(2)
+time.sleep(2)
+#Hire()
+ScanEnemies()
+#SpawnGoblins()
